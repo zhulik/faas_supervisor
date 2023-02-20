548353ab706e153da8c2e13cc40178baf8c23374
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-02-20 20:06:19 +0100

    Refactoring, add kubernetes client to container

--- lib/faas_supervisor/application.rb
+++ lib/faas_supervisor/application.rb
@@ -3,17 +3,50 @@
 class FaasSupervisor::Application
   include FaasSupervisor::Helpers
 
+  DEFAULT_KUBERNETES_HOST = "127.0.0.1"
+  DEFAULT_KUBERNETES_PORT = 8001
+
   option :openfaas_url, type: T::String
   option :openfaas_username, type: T::String
   option :openfaas_password, type: T::String
+
   option :prometheus_url, type: T::String
+
+  option :kubernetes_url, type: T::String
+  option :kubernetes_scheme, type: T::Coercible::String
+
   option :update_interval, type: T::Coercible::Integer, default: -> { 10 }
+
   option :metrics_server_port, type: T::Coercible::Integer, default: -> { 8080 }
 
   inject :openfaas
   inject :metrics_store
 
   class << self
+    def config # rubocop:disable Metrics/MethodLength
+      kubernetes_host = ENV.fetch("KUBERNETES_SERVICE_HOST", DEFAULT_KUBERNETES_HOST)
+      kubernetes_port = ENV.fetch("KUBERNETES_SERVICE_PORT", DEFAULT_KUBERNETES_PORT)
+
+      kubernetes_scheme = kubernetes_host == DEFAULT_KUBERNETES_HOST ? :http : :https
+      {
+        openfaas_url: ENV.fetch("OPENFAAS_URL"),
+        openfaas_username: ENV.fetch("OPENFAAS_USERNAME"),
+        openfaas_password: ENV.fetch("OPENFAAS_PASSWORD"),
+
+        prometheus_url: ENV.fetch("PROMETHEUS_URL", "http://127.0.0.1:9090"),
+
+        update_interval: ENV.fetch("SUPERVISOR_UPDATE_INTERVAL", "10"),
+
+        metrics_server_port: ENV.fetch("SUPERVISOR_METRICS_SERVER_PORT", "8080"),
+        kubernetes_url: "#{kubernetes_host}:#{kubernetes_port}",
+        kubernetes_scheme:
+      }
+    end
+
+    def build
+      new(**config)
+    end
+
     def instance = @@instance
 
     def [](key) = instance[key]
@@ -89,5 +122,6 @@ class FaasSupervisor::Application
                                                        password: openfaas_password))
     container.register(:prometheus, Prometheus::ApiClient.client(url: prometheus_url))
     container.register(:metrics_store, Metrics::Store.new)
+    container.register(:kubernetes, Kubernetes::Client.new(host: kubernetes_url, scheme: kubernetes_scheme))
   end
 end
