5a75e594d57060dd34045af94cd2dc7dc47b5b88
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-02-25 14:56:03 +0100

    Use Zill as kubernetes openapi client

--- lib/faas_supervisor/deployer.rb
+++ lib/faas_supervisor/deployer.rb
@@ -26,8 +26,26 @@ class FaasSupervisor::Deployer
   # TODO: add timeout
   def cycle
     debug { "Checking..." }
-    info { kubernetes.deployment(function.name, "openfaas").spec.selector.matchLabels.pretty_inspect }
+    info { images }
   rescue StandardError => e
     warn(e)
+  end
+
+  memoize def deployment = kubernetes.apps_v1_api.read_apps_v1_namespaced_deployment(function.name, function.namespace)
+
+  def pods
+    label_selector = deployment.spec.selector.match_labels.map { "#{_1}=#{_2}" }.join(",")
+    kubernetes.core_v1_api.list_core_v1_namespaced_pod(function.namespace, label_selector:).items
+  end
+
+  def images
+    pods.flat_map do |pod|
+      pod.status.container_statuses.map do |status|
+        {
+          image: status.image,
+          image_id: status.image_id
+        }
+      end
+    end.uniq
   end
 end
