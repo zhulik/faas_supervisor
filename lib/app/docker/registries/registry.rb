24498186a4dc0fd0c6b663cadee929108b2e464c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2023-03-04 00:05:53 +0100

    Cache tokens per repository

--- lib/app/docker/registries/registry.rb
+++ lib/app/docker/registries/registry.rb
@@ -14,17 +14,32 @@ class App::Docker::Registries::Registry
     raise ArgumentError, "reference must have non-nil tag" if reference.tag.nil?
 
     throttler.wait
-    connection.get("/v2/#{reference.full_name}/manifests/#{reference.tag}", {},
-                   DEFAULT_HEADERS.merge(Authorization: "Bearer #{token(reference)}"))
+    connection.head("/v2/#{reference.full_name}/manifests/#{reference.tag}", {},
+                    DEFAULT_HEADERS.merge(Authorization: "Bearer #{cached_token(reference)}"))
               .headers["docker-content-digest"]
   end
 
   private
 
-  memoize def throttler = Async::Throttler.new(5, 10)
+  memoize def throttler = Async::Throttler.new(1, 10)
+  memoize def cache = Hash.new { _1[_2] = {} }
 
   memoize def connection
     raise NotImplementedError
+  end
+
+  def cached_token(reference) = with_cache(reference, duration: 50) { token(_1) }
+
+  def with_cache(id, duration:, &)
+    existing = cache[id]
+
+    existing[:task]&.wait
+    return existing[:value] if existing[:value] && Time.now - existing[:created_at] < duration
+
+    Async { existing.merge!(value: yield(id), created_at: Time.now) }.tap do |task|
+      existing[:task] = task
+    end
+    with_cache(id, duration:, &)
   end
 
   def token(reference) = raise NotImplementedError
